d8313fae47044b19c63fe6eec1fd8b643d0e7741
Liviu Octavian Heinrich <76899634+LOH07@users.noreply.github.com>
2021-03-04 15:27:39 +0000

    Update ping_servers.py

--- ping_servers.py
+++ ping_servers.py
@@ -8,11 +8,14 @@ servers = ['0.0.0.1','127.0.0.1']
 # so the len(pings) should be equal to the len(servers)
 pings = [0,0]
 
+# the number of seconds to wait between pings (ADJUST AS NEEDED!)
+seconds = 30
+
 # this is the MAIN LOOP that will run until someone presses 'Ctrl'+'C'
 while True:
     print('\nPress "Ctrl" + "C" to stop this program\n')
     
-    # this is the loop that will run every X seconds (see 'time.sleep()' below)
+    # this is the loop that will run every X seconds (see 'seconds' above and 'time.sleep()' below)
     for i in range(0,len(servers)):
         print(f'Sending pings to server {servers[i]} ...\n')
         
@@ -42,11 +45,11 @@ while True:
             else:
                 pass
     
-    print('Waiting 10 seconds ...')
+    print(f'Waiting {seconds} seconds ...')
     
-    # this command creates a break of 10 seconds between the FOR LOOPS
-    # so the 'servers' are pinged every 10 seconds and THIS CAN BE ADJUSTED as needed
-    time.sleep(10)
+    # this command creates a break of X seconds between the FOR LOOPS (see variable 'seconds' above)
+    # so the 'servers' are pinged every X seconds and THIS CAN BE ADJUSTED as needed
+    time.sleep(seconds)
     
     # this command CLEARS the terminal (the screen)
     os.system('clear')
